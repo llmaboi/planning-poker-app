d4bd1f4805ce61233e0a5dac4789367ce7b2376a
Sam Peterson <samllama55@gmail.com>
2022-10-16 21:38:25 +0200

    CHANGE: Comment.

--- src/hooks/login.hooks.ts
+++ src/hooks/login.hooks.ts
@@ -1,6 +1,20 @@
 import { signInAnonymously } from '@firebase/auth';
 import { useMutation } from 'react-query';
 
+
+// TODO: Next set of changes:
+//   ADD:
+//     -- More strict TS
+//     -- Alias paths
+//     -- Api files to contain core firestore logic
+//   Remove:
+//     -- @react-query-firebase/auth
+//     -- @react-query-firebase/firestore
+//   Modify:
+//     -- All endpoints to be base react-query calls
+//     -- hooks to use "api files"
+
+
 function useLoginMutation() {
   return useMutation(signInAnonymously);
 }
